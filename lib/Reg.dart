5686fadc9abb5e3ac399a0709fc7ed3f409a82e0
raj vaya <rajkumarvaya@gmail.com>
2019-10-13 18:38:52 +0530

    Final Commit With APK

--- lib/Reg.dart
+++ lib/Reg.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 class Reg extends StatefulWidget {
   @override
@@ -6,8 +8,98 @@ class Reg extends StatefulWidget {
 }
 
 class _RegState extends State<Reg> {
+
+
+   TextEditingController C = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("Sing up"),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 8,),
+            TextField(
+              style:
+              TextStyle(fontSize: 16),
+              decoration: InputDecoration(
+                labelText: 'Email',
+                focusColor: Colors.black,
+                border: OutlineInputBorder(),
+              ),
+
+
+            ),
+            SizedBox(height: 8,),
+            TextField(
+              style:
+              TextStyle(fontSize: 16),
+              decoration: InputDecoration(
+                labelText: 'Name',
+                focusColor: Colors.black,
+                border: OutlineInputBorder(),
+              ),
+
+
+            ),
+            SizedBox(height: 8,),
+            TextField(
+              controller: C ,
+              style:
+              TextStyle(fontSize: 16),
+              decoration: InputDecoration(
+                labelText: 'Phone Number',
+                focusColor: Colors.black,
+                border: OutlineInputBorder(),
+              ),
+
+
+            ),
+            SizedBox(height: 8,),
+           Container(
+             width: MediaQuery.of(context).size.width,
+               child: RaisedButton(
+                 color: Colors.blue,
+                   child: Text("Submit",style: TextStyle(color: Colors.white,fontSize: 18),),
+                   onPressed: (){
+
+
+                      if(C.text.length>=10)
+                        {
+                          Fluttertoast.showToast(
+                              msg: "Thank You We\'ll Contact You Soon",
+                              toastLength: Toast.LENGTH_LONG,
+                              gravity: ToastGravity.CENTER,
+                              timeInSecForIos: 1,
+
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                          );
+
+                          Navigator.of(context).pop();
+
+                        }
+
+                        else{
+
+                        Fluttertoast.showToast(
+                            msg: "Enter All The Details",
+                            toastLength: Toast.LENGTH_LONG,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIos: 1,
+                            textColor: Colors.white,
+                            fontSize: 16.0
+                        );
+
+
+                      }
+               }))
+
+          ],
+        ),
+      ),
+      
+    );
   }
 }
--- lib/map_view.dart
+++ lib/map_view.dart
@@ -3,6 +3,8 @@ import 'package:permission/permission.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import 'Reg.dart';
+
 class MapView extends StatefulWidget {
   @override
   _MapViewState createState() => _MapViewState();
@@ -29,45 +31,37 @@ class _MapViewState extends State<MapView> {
       appBar: AppBar(
         title: Center(child: Text("Water on Tap")),
       ),
-      drawer: Drawer(
-        child: Stack(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                UserAccountsDrawerHeader(
-                    accountName: Text("NOOB"),
-                    accountEmail: Text("NOOB@noob.com"))
-              ],
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                color: Colors.blueAccent,
-                child: ListTile(
-                  title: Text("Become a Water Refiller",style: TextStyle(
-                    fontSize: 18
-                  ),),
-                ),
-              ),
 
-            )
-          ],
-        ),
-      ),
       body: GoogleMap(
         mapType: MapType.normal,
         myLocationButtonEnabled: true,
         myLocationEnabled: true,
         initialCameraPosition: _kGooglePlex,
-        markers: {Marker1},
+        markers: {
+          Marker1,
+          Marker2,
+          Marker3,
+          Marker4,
+          Marker5,
+          Marker6,
+          Marker7,
+          Marker8,
+          Marker08,
+          Marker9,
+          Marker10,
+          Marker11
+        },
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+        onPressed: () {
+          Navigator.push(context,
+              new MaterialPageRoute(builder: (context) => Reg()));
+        },
+        label: Text('Become a Partner'),
+        icon: Icon(Icons.person_add),
       ),
     );
   }
@@ -75,7 +69,103 @@ class _MapViewState extends State<MapView> {
   Marker Marker1 = Marker(
     markerId: MarkerId('gramercy'),
     position: LatLng(19.104498, 72.836024),
-    infoWindow: InfoWindow(title: 'RAJ VAYA', snippet: "hellow"),
+    infoWindow: InfoWindow(title: 'NMIMS ', snippet: "HOT WATER "),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+  Marker Marker2 = Marker(
+    markerId: MarkerId('gramercy2'),
+    position: LatLng(19.104498, 72.836024),
+    infoWindow: InfoWindow(title: 'Andheri Station', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+  Marker Marker3 = Marker(
+    markerId: MarkerId('gramercy3'),
+    position: LatLng(19.099618, 72.843952),
+    infoWindow: InfoWindow(title: 'Vile Parle Station ', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+
+  Marker Marker4 = Marker(
+    markerId: MarkerId('gramercy4'),
+    position: LatLng(19.082799, 72.841491),
+    infoWindow: InfoWindow(title: 'Santacruz Station', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+  Marker Marker5 = Marker(
+    markerId: MarkerId('gramercy5'),
+    position: LatLng(19.103892, 72.835930),
+    infoWindow: InfoWindow(title: 'UCO Bank', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+
+  Marker Marker6 = Marker(
+    markerId: MarkerId('gramercy6'),
+    position: LatLng(19.103110, 72.835924),
+    infoWindow: InfoWindow(title: 'Naskhtra', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+
+  Marker Marker7 = Marker(
+    markerId: MarkerId('gramercy7'),
+    position: LatLng(19.103634, 72.834369),
+    infoWindow: InfoWindow(title: 'Sunridge Hospital', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+
+  Marker Marker8 = Marker(
+    markerId: MarkerId('gramercy6'),
+    position: LatLng(19.104257, 72.832325),
+    infoWindow: InfoWindow(title: 'Juhu Joggers Park', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+
+  Marker Marker08 = Marker(
+    markerId: MarkerId('gramercy8'),
+    position: LatLng(19.100777, 72.827496),
+    infoWindow: InfoWindow(title: 'Starbucks Juhu', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+  Marker Marker9 = Marker(
+    markerId: MarkerId('gramercy9'),
+    position: LatLng(19.108171, 72.833516),
+    infoWindow: InfoWindow(title: 'Jain Temple', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+
+  Marker Marker10 = Marker(
+    markerId: MarkerId('gramercy10'),
+    position: LatLng(19.100411, 72.840746),
+    infoWindow: InfoWindow(title: 'McDonald\'s', snippet: "cool water"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueAzure,
+    ),
+  );
+  Marker Marker11 = Marker(
+    markerId: MarkerId('gramercy11'),
+    position: LatLng(19.109941, 72.837501),
+    infoWindow: InfoWindow(
+        title: 'Mukesh Patel School Of Technology Management & Engineering',
+        snippet: "cool water"),
     icon: BitmapDescriptor.defaultMarkerWithHue(
       BitmapDescriptor.hueAzure,
     ),
